3c0aae685725f1e573b04cf741beb1ef6cb3a6da
Zagiltalc <zachary.13281@gmail.com>
2026-02-27 16:54:23 -0500

    added link task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -25,6 +25,8 @@ HTTPS_ENVIRON = {'wsgi.url_scheme': 'https'}
 ######################################################################
 #  T E S T   C A S E S
 ######################################################################
+
+
 class TestAccountService(TestCase):
     """Account Service Tests"""
 
@@ -177,17 +179,17 @@ class TestAccountService(TestCase):
         self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
     def test_security_headers(self):
-            """It should return security headers"""
-            response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
-            self.assertEqual(response.status_code, status.HTTP_200_OK)
-            headers = {
-                'X-Frame-Options': 'SAMEORIGIN',
-                'X-Content-Type-Options': 'nosniff',
-                'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
-                'Referrer-Policy': 'strict-origin-when-cross-origin'
-            }
-            for key, value in headers.items():
-                self.assertEqual(response.headers.get(key), value)
+        """It should return security headers"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        headers = {
+            'X-Frame-Options': 'SAMEORIGIN',
+            'X-Content-Type-Options': 'nosniff',
+            'Content-Security-Policy': 'default-src \'self\'; object-src \'none\'',
+            'Referrer-Policy': 'strict-origin-when-cross-origin'
+        }
+        for key, value in headers.items():
+            self.assertEqual(response.headers.get(key), value)
 
     def test_cors_security(self):
         """It should return a CORS header"""
